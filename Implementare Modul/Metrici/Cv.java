a58df856b0d9de871621d8e5c0260fda0910f0ee
Tofan Cosmin-Alexandru <tofan_cosmin@yahoo.com>
2017-05-28 11:57:18 +0300

    Update Cv.java

--- Implementare Modul/Metrici/Cv.java
+++ Implementare Modul/Metrici/Cv.java
@@ -11,33 +11,120 @@ import java.util.Map;
 /**
  *
  * @author Mihalachi Bogdan-Marian, Balan Sabin-Marian, Ababei Bianca-Georgiana,
- * anul 2, grupa A1
+ * Ioja Petru-Alexandru, anul 2, grupa A1
  */
 public class Cv {
 
-    private static int count = 0;
     private int ID;
     private String name;
     private String dateOfBirth;
-    private Map<String, Integer> experience;
-    private Map<String, Integer> programmingLanguages;
+    /* v */
     private int age;
-    private Map<String, Integer> personalQualities;
-    private int projects;
-    private Map<String, Integer> languages;
+    private String address;
+    private String phoneNumber;
+    private String email;
+    /* v */
+    private int experienceYears;
+    private String idealJob;
+    /* v */
+    private Map<String, Integer> experience;
+    /* v */
+    private int[] studies; // studies[0] - nivelul de studiu, studies[1] - numarul de ani activi de la acel nivel
+    /* v */
+    private String[] technicalSkills;
+    /* v */
+    private String[] softSkills;
 
-    /**
-     * @return the dateOfBirth
-     */
-    public String getDateOfBirth() {
-        return dateOfBirth;
+    public Cv() {
+        this.ID = 0;
+        this.name = "";
+        this.dateOfBirth = "";
+        this.age = 0;
+        this.address = "";
+        this.phoneNumber = "";
+        this.email = "";
+        this.idealJob = "";
+        this.experienceYears = 0;
+        this.studies = new int[2];
+        this.experience = new HashMap<>();
+        /*this.technicalSkills = new String[100];
+        this.softSkills = new String[100];*/
+    }
+
+    public Cv(int ID,
+            String name,
+            String dateOfBirth,
+            int age,
+            String address,
+            String phoneNumber,
+            String email,
+            int experienceYears,
+            String idealJob,
+            Map<String, Integer> experience,
+            int[] studies,
+            String[] technicalSkills,
+            String[] softSkills) {
+        this.ID = ID;
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+        this.age = age;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.idealJob = idealJob;
+        this.experienceYears = experienceYears;
+        this.studies = new int[2];
+        System.arraycopy(studies, 0, this.studies, 0, studies.length);
+        this.experience = new HashMap<>();
+        for (Map.Entry<String, Integer> entry : experience.entrySet()) {
+            this.experience.put(entry.getKey(), entry.getValue());
+        }
+        this.technicalSkills = new String[technicalSkills.length];
+        this.softSkills = new String[softSkills.length];
+        System.arraycopy(technicalSkills, 0, this.technicalSkills, 0, technicalSkills.length);
+        System.arraycopy(softSkills, 0, this.softSkills, 0, softSkills.length);
     }
 
     /**
-     * @param dateOfBirth the dateOfBirth to set
+     * @return the technicalSkills
      */
-    public void setDateOfBirth(String dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
+    public String[] getTechnicalSkills() {
+        return technicalSkills;
+    }
+
+    /**
+     * @param technicalSkills the technicalSkills to set
+     */
+    public void setTechnicalSkills(String[] technicalSkills) {
+        this.technicalSkills = technicalSkills;
+    }
+    
+    /**
+     * @return the age
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * @param age the age to set
+     */
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    /**
+     * @return the softSkills
+     */
+    public String[] getSoftSkills() {
+        return softSkills;
+    }
+
+    /**
+     * @param softSkills the softSkills to set
+     */
+    public void setSoftSkills(String[] softSkills) {
+        this.softSkills = softSkills;
     }
 
     /**
@@ -69,6 +156,90 @@ public class Cv {
     }
 
     /**
+     * @return the dateOfBirth
+     */
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    /**
+     * @param dateOfBirth the dateOfBirth to set
+     */
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    /**
+     * @return the address
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * @param address the address to set
+     */
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    /**
+     * @return the phoneNumber
+     */
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    /**
+     * @param phoneNumber the phoneNumber to set
+     */
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the experienceYears
+     */
+    public int getExperienceYears() {
+        return experienceYears;
+    }
+
+    /**
+     * @param experienceYears the experienceYears to set
+     */
+    public void setExperienceYears(int experienceYears) {
+        this.experienceYears = experienceYears;
+    }
+
+    /**
+     * @return the idealJob
+     */
+    public String getIdealJob() {
+        return idealJob;
+    }
+
+    /**
+     * @param idealJob the idealJob to set
+     */
+    public void setIdealJob(String idealJob) {
+        this.idealJob = idealJob;
+    }
+
+    /**
      * @return the experience
      */
     public Map<String, Integer> getExperience() {
@@ -83,102 +254,72 @@ public class Cv {
     }
 
     /**
-     * @return the programmingLanguages
+     * @return the studies
      */
-    public Map<String, Integer> getProgrammingLanguages() {
-        return programmingLanguages;
+    public int[] getStudies() {
+        return studies;
     }
 
     /**
-     * @param programmingLanguages the programmingLanguages to set
+     * @param studies the studies to set
      */
-    public void setProgrammingLanguages(Map<String, Integer> programmingLanguages) {
-        this.programmingLanguages = programmingLanguages;
+    public void setStudies(int[] studies) {
+        this.studies = studies;
     }
 
-    /**
-     * @return the age
-     */
-    public int getAge() {
-        return age;
-    }
+    public void display() {
+        System.out.print("ID: " + this.ID + "\n");
+        System.out.print("Nume: " + this.name + "\n");
+        System.out.print("Data nastere: " + this.dateOfBirth + "\n");
+        System.out.print("Varsta: " + this.getAge() + "\n");
+        System.out.print("Adresa: " + this.address + "\n");
+        System.out.print("Numar de telefon: " + this.phoneNumber + "\n");
+        System.out.print("Email: " + this.email + "\n");
+        System.out.print("Job ideal: " + this.idealJob + "\n");
+        System.out.print("Ani experienta totala: " + this.experienceYears + "\n");
+        System.out.print("Experienta pe domenii: " + "\n");
+        for (int i = 0; i < this.experience.size(); i++) {
+            if (this.experience.get(this.experience.keySet().toArray()[i].toString()) != 0) {
+                System.out.print("\t" + this.experience.keySet().toArray()[i] + " --> " + this.experience.get(this.experience.keySet().toArray()[i].toString()) + " ani " + "\n");
+            }
+        }
 
-    /**
-     * @param age the age to set
-     */
-    public void setAge(int age) {
-        this.age = age;
-    }
+        System.out.print("Studii: " + "\n");
+        switch (this.studies[0]) {
+            case 0:
+                System.out.print("\tLiceu: " + this.studies[1] + " ani" + "\n");
+                break;
+            case 2:
+                System.out.print("\tLiceu: " + 4 + " ani" + "\n");
+                System.out.print("\tLicenta: " + this.studies[1] + " ani" + "\n");
+                break;
+            case 4:
+                System.out.print("\tLiceu: " + 4 + " ani" + "\n");
+                System.out.print("\tLicenta: " + 4 + " ani" + "\n");
+                System.out.print("\tMaster: " + this.studies[1] + " ani" + "\n");
+                break;
+            case 6:
+                System.out.print("\tLiceu: " + 4 + " ani" + "\n");
+                System.out.print("\tLicenta: " + 4 + " ani" + "\n");
+                System.out.print("\tMaster: " + 3 + " ani" + "\n");
+                System.out.print("\tDoctorat: " + this.studies[1] + " ani" + "\n");
+                break;
+            default:
+                break;
+        }
 
-    /**
-     * @return the personalQualities
-     */
-    public Map<String, Integer> getPersonalQualities() {
-        return personalQualities;
-    }
-
-    /**
-     * @param personalQualities the personalQualities to set
-     */
-    public void setPersonalQualities(Map<String, Integer> personalQualities) {
-        this.personalQualities = personalQualities;
-    }
-
-    /**
-     * @return the projects
-     */
-    public int getProjects() {
-        return projects;
-    }
-
-    /**
-     * @param projects the projects to set
-     */
-    public void setProjects(int projects) {
-        this.projects = projects;
-    }
-
-    /**
-     * @return the languages
-     */
-    public Map<String, Integer> getLanguages() {
-        return languages;
-    }
-
-    /**
-     * @param languages the languages to set
-     */
-    public void setLanguages(Map<String, Integer> languages) {
-        this.languages = languages;
-    }
-
-    public Cv() {
-        ID = ++count;
-        this.name = "";
-        this.dateOfBirth = "";
-        this.experience = new HashMap<>();
-        this.languages = new HashMap<>();
-        this.personalQualities = new HashMap<>();
-        this.programmingLanguages = new HashMap<>();
-        this.age = 0;
-        this.projects = 0;
-    }
-
-    public Cv(String name,
-            String dateOfBirth,
-            Map<String, Integer> experience,
-            Map<String, Integer> programmingLanguages,
-            int age,
-            Map<String, Integer> personalQualities,
-            int projects,
-            Map<String, Integer> languages) {
-        this.name = name;
-        this.dateOfBirth = dateOfBirth;
-        this.experience = experience;
-        this.languages = languages;
-        this.personalQualities = personalQualities;
-        this.programmingLanguages = programmingLanguages;
-        this.age = age;
-        this.projects = projects;
+        System.out.print("Technical skills: " + "\n");
+        for (String technicalSkill : technicalSkills) {
+            if (technicalSkill != null) {
+                System.out.print("\t" + technicalSkill + "\n");
+            }
+        }
+        System.out.print("Soft skills: " + "\n");
+        for (String softSkill : softSkills) {
+            if (softSkill != null) {
+                System.out.print("\t" + softSkill + "\n");
+            }
+        }
+        System.out.print("\n");
     }
 }
